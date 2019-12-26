ca1886c085810a779c37f2bdbed208d657c15d94
Carsten Dolar <Carsten.Dolar@gmail.com>
2019-12-26 20:47:04 +0100

    update firmware example

--- examples/WordclockFirmware/WordclockFirmware.ino
+++ examples/WordclockFirmware/WordclockFirmware.ino
@@ -13,9 +13,31 @@ wordclock_t mywordclock;
 matrixEmu_t matrixEmu;
 
 // three element pixels, in different order and speeds
-NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(wordclock_t::SIZE);
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> matrix(wordclock_t::SIZE);
 
 using namespace wordclock;
+
+void showCurrentTime()
+{
+    matrix.ClearTo(RgbColor(0));
+    matrixEmu.clear();
+    DateTime now = rtc.now();
+    uint8_t colorSaturation = config.maxBrightness;
+    if (config.brightnessMode == timeBrightness) 
+    {
+        if ((now.hour() < config.brightnessStartHour) || (now.hour() >= config.brightnessEndHour))
+        {
+            colorSaturation = config.minBrightness;
+        }
+    }
+    RgbColor ledColor(colorSaturation);
+    mywordclock.setTime(now.hour(), now.minute(), matrixEmu, ledColor);
+    mywordclock.setTime(now.hour(), now.minute(), matrix, ledColor);
+    matrix.Show();
+    matrixEmu.print();
+}
+
+void 
 
 void setup()
 {
@@ -39,25 +61,42 @@ void setup()
 
     Serial.println("Setting up Webserver");
     webserver.init();
+
+    // initializing led matrix and run a short test
+    uint8_t colorSaturation = config.maxBrightness;
+    RgbColor red(colorSaturation, 0, 0);
+    RgbColor green(0, colorSaturation, 0);
+    RgbColor blue(0, 0, colorSaturation);
+    RgbColor white(colorSaturation);
+    RgbColor black(0);
+
+    matrix.Begin();
+    while(!matrix.CanShow()){}
+    matrix.Show();
+    matrix.SetPixelColor(0,red);
+    matrix.SetPixelColor(1,green);
+    matrix.SetPixelColor(2,blue);
+    matrix.SetPixelColor(3,white);
+    while(!matrix.CanShow()){}
+    matrix.Show();
+    for (int i=0; i<wordclock_t::SIZE; ++i)
+    {
+        delay(300);
+        matrix.RotateRight(1);
+        while(!matrix.CanShow()){}
+        matrix.Show();
+    }
+
+    showCurrentTime();
 }
 
 
 void loop()
 {
-    for (uint8_t h=0; h<23; ++h)
+    webserver.loop();
+
+    if (rtc.hasNewTime())
     {
-        for (uint8_t m=0; m<60; m+=5)
-        {
-            Serial.print("Current Hours ");
-            Serial.print(h);
-            Serial.print(":");
-            Serial.print(m);
-            Serial.println("");
-            Serial.flush();
-            matrixEmu.clear();
-            mywordclock.setTime(h, m, matrixEmu, matrixEmu_t::StdColor);
-            matrixEmu.print();
-            delay(100);
-        }
+        showCurrentTime();
     }
 }
